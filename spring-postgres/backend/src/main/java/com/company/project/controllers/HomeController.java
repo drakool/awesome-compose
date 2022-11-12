229ad2c6db1682d01f974ce36d800c9ed015e5f2
Mohamed Hany <mohamed.hany@segmatek.com>
2022-11-12 13:20:49 +0200

    updated spring-postgres to use another postgres container in an external network

--- spring-postgres/backend/src/main/java/com/company/project/controllers/HomeController.java
+++ spring-postgres/backend/src/main/java/com/company/project/controllers/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -15,7 +18,15 @@ public class HomeController {
 
     @GetMapping("/")
     public String showHome(String name, Model model) {
-        Greeting dockerGreeting = repository.findById(1).orElse(new Greeting("Not Found 😕"));
+        Greeting dockerGreeting = repository.findById(2).orElse(new Greeting("Not Found 😕"));
+        model = model.addAttribute("name", dockerGreeting.getName());
+        return "home";
+    }
+
+
+    @GetMapping("/{id}")
+    public String getGreetingNameById(String name,@PathVariable Integer id, Model model) {
+        Greeting dockerGreeting = repository.findById(id).orElse(new Greeting("Not Found 😕"));
         model = model.addAttribute("name", dockerGreeting.getName());
         return "home";
     }
--- spring-postgres/backend/src/main/resources/data.sql
+++ spring-postgres/backend/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO GREETINGS(name) values ('Docker');
+INSERT INTO GREETINGS(name) values ('Hany');
